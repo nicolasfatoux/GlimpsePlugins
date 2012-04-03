6b00deaca3dd21ac46e711d520f68100175586ca
Nicolas Fatoux <nicolas.fatoux@gmail.com>
2012-04-03 22:49:10 +0200

    Hello IIS Log plugin for Glimpse !

---
+++ GlimpsePlugins.IISLog/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glimpse.Core.Extensibility;
+using System.Web;
+
+namespace GlimpsePlugins.IISLog
+{
+    [GlimpsePlugin]
+    public class Plugin : IGlimpsePlugin
+    {
+        public object GetData(HttpContextBase context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { return "IIS Log"; }
+        }
+
+        public void SetupInit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
