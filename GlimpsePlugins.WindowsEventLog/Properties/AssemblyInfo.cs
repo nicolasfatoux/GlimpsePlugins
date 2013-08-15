0237c16355b6169d33ab5a0560a6bb6bb4d0352a
Nicolas Fatoux <nicolas.fatoux@gmail.com>
2013-08-15 19:37:03 +0200

    Preparing version 1.2

--- GlimpsePlugins.WindowsEventLog/Properties/AssemblyInfo.cs
+++ GlimpsePlugins.WindowsEventLog/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GlimpsePlugins.WindowsEventLog")]
+[assembly: AssemblyTitle("Windows Event Log plugin for Glimpse")]
 [assembly: AssemblyDescription("A Glimpse plugin that displays Windows Event Log entries from the server.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicolas Fatoux")]
-[assembly: AssemblyProduct("GlimpsePlugins.WindowsEventLog")]
-[assembly: AssemblyCopyright("Copyright © Nicolas Fatoux 2012")]
+[assembly: AssemblyProduct("Windows Event Log plugin for Glimpse")]
+[assembly: AssemblyCopyright("Copyright © Nicolas Fatoux 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
