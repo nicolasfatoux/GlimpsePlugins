55cf3614277d65933ca491d0161279360cbb63b7
NicolasFatoux <nicolas.fatoux@gmail.com>
2014-03-01 17:18:44 +0100

    New test project

---
+++ NfxLab.GlimpsePlugins.Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NfxLab.GlimpsePlugins.Test.Startup))]
+namespace NfxLab.GlimpsePlugins.Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
